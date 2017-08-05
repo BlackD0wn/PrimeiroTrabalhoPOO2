606391e9a619fdde591196782417e5e8c9a602ee
Bruno <anjobruno92@gmail.com>
2017-08-05 20:00:37 -0300

    Adicionado Operario

---
+++ src/entidades/Operario.java
@@ -0,0 +1,27 @@
+package entidades;
+
+public class Operario extends Funcionario {
+
+	private String tipo;
+	
+	public Operario(String nome, String dataAdmissao, float salario, String tipo) {
+		super(nome, dataAdmissao, salario);
+		this.tipo = tipo;
+	}
+
+	@Override
+	public float getBonificacao() {
+		return (float) (this.getSalario() * 0.01);
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
