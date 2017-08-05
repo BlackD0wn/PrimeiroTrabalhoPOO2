26335d4b1db075a98913065da7638f44dd965443
Bruno <anjobruno92@gmail.com>
2017-08-05 19:44:26 -0300

    Adicionado Diretor

---
+++ src/entidades/Diretor.java
@@ -0,0 +1,17 @@
+package entidades;
+
+public class Diretor extends Funcionario {
+
+	private String setorGerencia;
+	
+	public Diretor(String nome, String dataAdmissao, String setorGerencia, float salario) {
+		super(nome, dataAdmissao, salario);
+		this.setorGerencia = setorGerencia;
+	}
+
+	@Override
+	public float getBonificacao() {
+		return (float) (this.getSalario() * 0.10);
+	}
+
+}
--- src/entidades/Funcionario.java
+++ src/entidades/Funcionario.java
@@ -3,14 +3,22 @@ package entidades;
 public abstract class Funcionario extends Pessoa{
 
 	private String dataAdmissao;
+	private float salario;
 	
-	public Funcionario(String nome, String dataAdmissao) {
+	public Funcionario(String nome, String dataAdmissao, float salario) {
 		super(nome);
 		this.dataAdmissao = dataAdmissao;
+		this.salario = salario;
 	}
 	
 	public String getDataAdmissao(){
 		return this.dataAdmissao;
 	}
+	
+	public float getSalario(){
+		return this.salario;
+	}
+	
+	public abstract float getBonificacao();
 
 }
