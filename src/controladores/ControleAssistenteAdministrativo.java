de09e0c0e5dfbd771e15f3bab8d3dc311a969639
Bruno <anjobruno92@gmail.com>
2017-08-05 20:59:14 -0300

    Adicionado controle assistente administrativo

---
+++ src/controladores/ControleAssistenteAdministrativo.java
@@ -0,0 +1,32 @@
+package controladores;
+
+import entidades.AssistenteAdministrativo;
+import interfaces.InterfaceDados;
+
+public class ControleAssistenteAdministrativo implements InterfaceDados {
+
+	private AssistenteAdministrativo assistente;
+	
+	public ControleAssistenteAdministrativo(AssistenteAdministrativo assistente) {
+		this.assistente = assistente;
+	}
+
+	@Override
+	public void salvar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void recuperar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void excluir() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
