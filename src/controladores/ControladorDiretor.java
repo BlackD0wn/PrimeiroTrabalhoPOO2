80286af30882c354015b04898a2b1a6f6ad9b730
Bruno <anjobruno92@gmail.com>
2017-08-05 20:54:41 -0300

    Adicionado controle diretor

---
+++ src/controladores/ControladorDiretor.java
@@ -0,0 +1,32 @@
+package controladores;
+
+import entidades.Diretor;
+import interfaces.InterfaceDados;
+
+public class ControladorDiretor implements InterfaceDados {
+
+	private Diretor diretor;
+	
+	public ControladorDiretor(Diretor diretor) {
+		this.diretor = diretor;
+	}
+
+	@Override
+	public void salvar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void recuperar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void excluir() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
