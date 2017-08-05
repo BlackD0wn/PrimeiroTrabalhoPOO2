a2bf425d65796eda4e262d6da2290d7b531fb613
Bruno <anjobruno92@gmail.com>
2017-08-05 20:45:27 -0300

    Adicionado controle gerente

---
+++ src/controladores/ControladorGerente.java
@@ -0,0 +1,33 @@
+package controladores;
+
+import entidades.Gerente;
+import interfaces.InterfaceDados;
+
+public class ControladorGerente implements InterfaceDados {
+
+	private Gerente gerente;
+	
+	public ControladorGerente(Gerente gerente) {
+		this.gerente = gerente;
+		
+	}
+
+	@Override
+	public void salvar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void recuperar() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void excluir() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
